21559fa5141321c189fbe6b4205dd68d84a46b63
apoorvjoshi <avjoshi@ncsu.edu>
2015-11-27 21:11:36 -0500

    Updated build-vm

---
+++ build-vm/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update Ubuntu
+    sudo apt-get update
+    sudo apt-get install -y make
+    sudo apt-get install -y gcc
+    
+# Install flash to run demo YouTube videos
+    sudo apt-get install -y flashplugin-installer
+
+# Remove unwanted icons from Launcher
+    cd /usr/share/applications && sudo rm -f libreoffice-writer.desktop libreoffice-calc.desktop libreoffice-impress.desktop ubuntu-software-center.desktop ubuntu-amazon-default.desktop
+
+# Copy VM files to Desktop
+    sudo chmod 777 /vagrant/*
+    unzip /vagrant/PREST_VM_Desktop_Files.zip -d /home/vagrant/Desktop/
+    chmod +x /home/vagrant/Desktop/*.desktop
+
+    cd ~/Desktop/prest_3_02
+    make -k
+    make -k altertest
+
+# Add Terminal to startup applications before reloading VM
+    mkdir /home/vagrant/.config/autostart/
+    mv /home/vagrant/Desktop/Terminal.desktop /home/vagrant/.config/autostart/
+
+#Reboot the system
+    sudo reboot
